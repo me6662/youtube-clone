c7ef204aa9156d48432bae02c1e190b5f4de380f
ydg <me6662@naver.com>
2020-07-26 23:30:16 +0900

    Add Middleware

--- index.js
+++ index.js
@@ -19,7 +19,28 @@ function handleHome(req, res) {
 // ES6 에서 새로 도입된 함수 표현식. (ARROW FUNCTION, 훨씬 보기좋다.)
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-app.get("/", handleHome); // "/" 는 home url 주소. , 브라우저가 get 함수를 실행했으므로 (user 요청) 응답해야됨.
+// Middleware 만들기 (request 와 응답 사이의 과정)
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next(); // 다음을 진행하라는 소리.
+};
+
+const betweenTest = (req, res, next) => {
+  console.log("I only test middleware procedure.");
+  next();
+};
+
+//app.get("/", betweenHome, handleHome); // "/" 는 home url 주소. , 브라우저가 get 함수를 실행했으므로 (user 요청) 응답해야됨.
+// betweenHome 은 request 와 respond 사이에 껴있음. 오직 "/" route 에만 작용.
+
+app.use(betweenHome); // 이렇게 하면, 모든 웹사이트 응답에 사이에 끼게 됨.
+// 여기서 순서가 중요한데 get 에서 route 를 찾기전에 이 middle ware 들이 실행되는 것이다.
+// request 가 와서 app 는 "/" 와 "/profile" route 를 찾기 전에 betweenHome 미들웨어를 무조건 실행하게 된다. 이게 더 뒤로 가거나 하면 안됨
+// 그리고 route 전에는 원하는 만큼의 middleware를 써도된다.
+
+app.get("/", handleHome);
+
+app.use(betweenTest);
 
 app.get("/profile", handleProfile); // "/profile" 이렇게 route 가 생성 (단순히 객체가 생성된다고 생각하면 될듯) 되고 handler 와 묶임.
 
