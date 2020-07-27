c63575a605f991b9db0baa75b4d0f8dcc43d716f
ydg <me6662@naver.com>
2020-07-27 22:56:55 +0900

    middleware cookie parser & body parser

--- index.js
+++ index.js
@@ -16,39 +16,19 @@ function handleHome(req, res) {
   // get,post 함수 핸들러는 argument 가 두개임 1. request object , 2. respond object.
   // request object : post 시 user 정보 (아이디 패스워드 등) 같은 것들 포함
   // respond object : server 에서 응답 할 때 사용되는 객체
-  //console.log(req);
   res.send("hello 시발놈아"); // 웹사이트처럼 작동하게 해주려면 html css 파일을 send 하면 됨.
 }
 
 // ES6 에서 새로 도입된 함수 표현식. (ARROW FUNCTION, 훨씬 보기좋다.)
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-// Middleware 만들기 (request 와 응답 사이의 과정)
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next(); // 다음을 진행하라는 소리.
-};
+app.use(cookieParser()); // User 로부터 쿠키를 받도록함.
+app.use(bodyParser.json()); //User 로부터 json 받을때 이해하도록함.
+app.use(bodyParser.urlencoded({ extended: true })); //User 로부터 Html 받을때 이해하도록함.
+app.use(helmet()); // NodeJS 보안 기능추가
+app.use(morgan("dev")); // Terminal 에 로그남김.
 
-const betweenTest = (req, res, next) => {
-  console.log("I only test middleware procedure.");
-  next();
-};
-
-//app.get("/", betweenHome, handleHome); // "/" 는 home url 주소. , 브라우저가 get 함수를 실행했으므로 (user 요청) 응답해야됨.
-// betweenHome 은 request 와 respond 사이에 껴있음. 오직 "/" route 에만 작용.
-
-app.use(morgan("dev"));
-app.use(helmet());
-
-/* Middle ware 가 연결을 끊는법
-middleware 가 get 전에 응답을 해버리면 연결이 성사가 안됨.
-*/
-
-const middleware = (req, res, next) => {
-  res.send("not happnening. by MW.");
-};
-
-app.get("/", middleware, handleHome);
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile); // "/profile" 이렇게 route 가 생성 (단순히 객체가 생성된다고 생각하면 될듯) 되고 handler 와 묶임.
 
